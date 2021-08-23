5766099a6204aac862a696dc57ad0cd2599b8f74
sigvant <bnsviv@gmail.com>
2021-08-23 20:42:30 -0300

    fixed tabloading, also added minimum date and separated retriveing tabs from memory into functions, the same was done to clear todos function

--- app.js
+++ app.js
@@ -23,11 +23,10 @@ const addTabButton = document.querySelector('.tab-button');
 const TabList = document.querySelector('.tab-list');
 const tab = document.querySelectorAll('.tab');
 
-
 //event listeners
-document.addEventListener('DOMContentLoaded', getTodos);
 document.addEventListener('DOMContentLoaded', loadTabs);
 document.addEventListener('DOMContentLoaded', activateTab);
+document.addEventListener('DOMContentLoaded', getTodayDate);
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
 filterOption.addEventListener('click', filterTodo);
@@ -75,7 +74,7 @@ function addTodo(event) {
 
 
     // add todo to the local storage
-    saveLocalTodos(newtodoobj);
+    // saveLocalTodos(newtodoobj);
     // CHECK MARK button
 
     //put todo in the active tab
@@ -101,12 +100,9 @@ function addTodo(event) {
 }
 
 function saveTodosinTab(todo) {
+    retrieveTabsFromMemory(); 
     const activeTab = document.querySelector('.tab-active');
     const tabName = activeTab.childNodes[0].innerText;
-    if(localStorage.getItem('tabs') === null) return;
-    else {
-        tabs = JSON.parse(localStorage.getItem('tabs'));
-    }    
     tabs.forEach(tab => {
         if(tab.name == tabName) {
             tab.todos.push(todo);
@@ -123,7 +119,7 @@ function deleteCheck(event) {
         const todo = item.parentElement;
         // animation
         todo.classList.add('fall');
-        removeLocalTodos();
+        removeLocalTodos(todo);
         todo.addEventListener('transitionend', () => {
             todo.remove();
         });
@@ -162,100 +158,69 @@ function filterTodo(event) {
     });
 }
 
-function saveLocalTodos(todo) {
-    //check -- do I already have things in there?
-    let todos;
-    if(localStorage.getItem('todos') === null) {
-        todos = [];
-    } else {
-        todos = JSON.parse(localStorage.getItem('todos'));
-    }
-    todos.push(todo);
-
-    localStorage.setItem('todos', JSON.stringify(todos));
-    
-}
-
 function getTodos() {
-    let todos;
-    if(localStorage.getItem('todos') === null) {
-        todos = [];
-    } else {
-        todos = JSON.parse(localStorage.getItem('todos'));
-    }
-    todos.forEach(function(todo) {
-        // todo DIV
-        const todoDiv = document.createElement('div');
-        todoDiv.classList.add('todo');
-        // create LI
-
-        // todo object instantiation
-
-        let name = todo.name;
-        let date = todo.date;
-        let priority = todo.priority;        
-
-        const newTodo = document.createElement('li');
-        newTodo.classList.add('todo-item');
-
-        const newTodoNameDiv = document.createElement('div');
-        newTodoNameDiv.textContent = name;
-        newTodoNameDiv.classList.add('todo-info-boxes-name')
-
-        const newTodoDateDiv = document.createElement('div');
-        newTodoDateDiv.textContent = date;
-        newTodoDateDiv.classList.add('todo-info-boxes-date')
-
-        const newTodoPriorityDiv = document.createElement('div');
-        newTodoPriorityDiv.textContent = priority;
-        newTodoPriorityDiv.classList.add('todo-info-boxes-priority')
+    retrieveTabsFromMemory();
+    
+    let tabName = document.querySelector('.tab-active').children[0].children[0].innerText;
+    
+    tabs.forEach(tab => {
+        if(tab.name == tabName) {
+            tab.todos.forEach(todo => {
+                // todo DIV
+                const todoDiv = document.createElement('div');
+                todoDiv.classList.add('todo');
+                // create LI
         
+                // todo object instantiation
         
-        newTodo.appendChild(newTodoNameDiv);
-        newTodo.appendChild(newTodoDateDiv);
-        newTodo.appendChild(newTodoPriorityDiv);
-
-        todoDiv.appendChild(newTodo);
-
-        const completedButton = document.createElement('button');
-        completedButton.textContent = '✔️';
-        completedButton.classList.add('complete-btn');
-        todoDiv.appendChild(completedButton);
+                let name = todo.name;
+                let date = todo.date;
+                let priority = todo.priority;        
         
-        // trash MARK button
-
-        const trashButton = document.createElement('button');
-        trashButton.textContent = '🗑️';
-        trashButton.classList.add('trash-btn');
-        todoDiv.appendChild(trashButton);
-
-        // append to list
-        todoList.appendChild(todoDiv);
-            
+                const newTodo = document.createElement('li');
+                newTodo.classList.add('todo-item');
+        
+                const newTodoNameDiv = document.createElement('div');
+                newTodoNameDiv.textContent = name;
+                newTodoNameDiv.classList.add('todo-info-boxes-name')
+        
+                const newTodoDateDiv = document.createElement('div');
+                newTodoDateDiv.textContent = date;
+                newTodoDateDiv.classList.add('todo-info-boxes-date')
+        
+                const newTodoPriorityDiv = document.createElement('div');
+                newTodoPriorityDiv.textContent = priority;
+                newTodoPriorityDiv.classList.add('todo-info-boxes-priority')
+                
+                
+                newTodo.appendChild(newTodoNameDiv);
+                newTodo.appendChild(newTodoDateDiv);
+                newTodo.appendChild(newTodoPriorityDiv);
+        
+                todoDiv.appendChild(newTodo);
+        
+                const completedButton = document.createElement('button');
+                completedButton.textContent = '✔️';
+                completedButton.classList.add('complete-btn');
+                todoDiv.appendChild(completedButton);
+                
+                // trash MARK button
+        
+                const trashButton = document.createElement('button');
+                trashButton.textContent = '🗑️';
+                trashButton.classList.add('trash-btn');
+                todoDiv.appendChild(trashButton);
+        
+                // append to list
+                todoList.appendChild(todoDiv);
+            })
+        }
     })
 }
 
-function removeLocalTodos() {
-    let todos;
-    if(localStorage.getItem('todos') === null) {
-        todos = [];
-    } else {
-        todos = JSON.parse(localStorage.getItem('todos'));
-    }
-    
-    // what I click is the div, now I want the li and the inner text
-    // so we can SPLICE it
-    const todoForTab = document.querySelector('.todo-info-boxes-name').innerText;
-    
-    todos = todos.filter(item => item.name != todoForTab );
-
-    // todos.splice(todos.indexOf(todoForTab), 1);
-    localStorage.setItem('todos', JSON.stringify(todos));
-
-    if(localStorage.getItem('tabs') === null) return;
-    else {
-        tabs = JSON.parse(localStorage.getItem('tabs'));
-    }      
+function removeLocalTodos(todo) {
+    const todoForTab = todo.childNodes[0].childNodes[0].innerText;
+    retrieveTabsFromMemory();
     tabs.forEach(item => {
         item.todos = item.todos.filter(item => item.name != todoForTab);
     })
@@ -307,6 +272,8 @@ function deleteSelectTab(event) {
     // delete todo
     if(item.classList[0] === 'trash-btn') {
         const tab = item.parentElement.parentElement;
+        // does not allow deletion when only one tab
+        if(tab.parentElement.childNodes.length === 1) return;
         // animation
         tab.classList.add('fall');
         removeLocalTab(tab);
@@ -322,6 +289,8 @@ function deleteSelectTab(event) {
         if(event.target.classList.contains('tab')) {
             tab.forEach(item => item.classList.remove('tab-active'));
             item.classList.add('tab-active');
+            clearTodos();
+            getTodos();
             
         }
         
@@ -330,24 +299,13 @@ function deleteSelectTab(event) {
 
 function saveLocalTab(tab) {
     //check -- do I already have things in there?
-    let tabs;
-    if(localStorage.getItem('tabs') === null) {
-        tabs = [];
-    } else {
-        tabs = JSON.parse(localStorage.getItem('tabs'));
-    }
+    retrieveTabsFromMemory();
     tabs.push(tab);
-
     localStorage.setItem('tabs', JSON.stringify(tabs));   
 }
 
 function loadTabs() {
-    let tabs;
-    if(localStorage.getItem('tabs') === null) {
-        tabs = [];
-    } else {
-        tabs = JSON.parse(localStorage.getItem('tabs'));
-    }
+    retrieveTabsFromMemory();
     tabs.forEach(function(tab){
         let name = tab.name;
         
@@ -376,12 +334,7 @@ function loadTabs() {
 }
 
 function removeLocalTab(tab) {
-    let tabs;
-    if(localStorage.getItem('tabs') === null) {
-        tabs = [];
-    } else {
-        tabs = JSON.parse(localStorage.getItem('tabs'));
-    }
+    retrieveTabsFromMemory();
     // what I click is the div, now I want the li and the inner text
     // so we can SPLICE it
     const tabIndex = tab.children[0].innerText;
@@ -395,5 +348,33 @@ function activateTab() {
     if(TabList.childNodes[0] === undefined) return;
     else {
         TabList.childNodes[0].classList.add('tab-active');    
+        getTodos();
     }
+}
+
+function clearTodos() {
+    document.querySelector('.todo-list').innerText = '';
+}
+
+function retrieveTabsFromMemory() {
+    if(localStorage.getItem('tabs') === null) return;
+    else {
+        tabs = JSON.parse(localStorage.getItem('tabs'));
+    }
+    return tabs;
+}
+
+function getTodayDate() {
+    let today = new Date();
+    let day = today.getDate();
+    let month = today.getMonth() + 1;
+    if(month < 10) {
+        month = `0${month}`;
+    }        
+    let year = today.getFullYear();
+    let todayDate = (`${year}-${month}-${day}`);
+    console.log(todayDate);
+
+    let dateInput = document.querySelector('.todo-date-input');
+    dateInput.setAttribute('min', todayDate);
 }
